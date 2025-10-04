b22877a15c40e74bdfcb2f5b77b211649d348d9f
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-10-05 01:44:20 +0900

    💬 comment: clarify notification purpose description in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function (event) {
   const data = event.data ? event.data.json() : {};
-  self.registration.showNotification(data.title || '알림', {
-    body: data.body || '',
+  self.registration.showNotification(data.title || '푸시 알림 테스트', {
+    body: data.body || '푸시 메시지 수신',
     icon: '/icons/icon-192x192.png',
   });
 });
