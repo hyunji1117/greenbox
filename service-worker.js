7be12c50e7e16bcec9410ada32c165ee5bdd017b
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-10-05 20:54:36 +0900

    ✨ feat: add notification click behavior in service worker

--- service-worker.js
+++ service-worker.js
@@ -3,5 +3,12 @@ self.addEventListener('push', function (event) {
   self.registration.showNotification(data.title || '푸시 알림 테스트', {
     body: data.body || '푸시 메시지 수신',
     icon: '/icons/icon-192x192.png',
+    badge: '/icons/icon-192x192.png',
   });
+});
+
+// 알림 클릭 동작
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
 });
